378d12416ed99626a83f0c23ce938f8f3f3163c5
Santhosh kannan <santhoshkannan1001@gmail.com>
2025-05-26 22:24:51 +0530

    updated

--- recipe-sharing-platform/server.js
+++ recipe-sharing-platform/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
